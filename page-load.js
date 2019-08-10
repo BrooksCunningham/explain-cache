396794770b28b460af30c9a106105da4be0869fb
Brooks Cunningham <22921241+BrooksCunningham@users.noreply.github.com>
2019-08-10 12:01:08 -0400

    Working with fetch is weird
    Major refector underway

--- page-load.js
+++ page-load.js
@@ -1,20 +1,51 @@
-fetch('/cache-headers-and-values.json')
+myList = document.querySelector('ul');
 
-fetch('./api/some.json')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-      });
+function getHeadersAndValues() {
+  fetch('./cache-headers-and-values.json')
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error("HTTP error, status = " + response.status);
     }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log('my json',json);
+    return json;
+  })
+  .catch(function(error) {
+    var p = document.createElement('p');
+    p.appendChild(
+      document.createTextNode('Error: ' + error.message)
+    );
+    document.body.insertBefore(p, myList);
   });
+  //return 'cat'
+};
+
+headersandValues = getHeadersAndValues();
+
+function updateHtmlWithValues(headersJson) {
+  for(var i = 0; i < headersJson.Headers.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = '<strong>' + headersJson.Headers[i].Name + '</strong>';
+    listItem.innerHTML +=' is known for doing <strong> ' + headersJson.Headers[i].Details + '</strong>.';
+    listItem.innerHTML +=' Find more info here: <strong>' + headersJson.Headers[i].URL + '</strong>';
+    myList.appendChild(listItem);
+  }
+}
+
+updateHtmlWithValues(headersandValues)
+
+function myFunction(){
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+  setTimeout(function(){
+    document.getElementById("demo").innerHTML = "Click me";
+  }, 2000);
+  inHeaders = document.getElementsByName('input_headers')[0].value;
+  console.log("some thing:  " + inHeaders);
+
+  if (inHeaders.includes('pragma')) {
+    console.log(inHeaders, inHeaders.includes('pragma'));
+  }
+
+};
