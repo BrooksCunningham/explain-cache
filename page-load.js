82c2573692a5827d04ea00aab34a4b0544494076
Brooks Cunningham <22921241+BrooksCunningham@users.noreply.github.com>
2019-08-12 22:02:22 -0400

    Buttons!

--- page-load.js
+++ page-load.js
@@ -19,7 +19,7 @@ function getHeadersAndValues() {
   .then(status)
   .then(json)
   .then(function(data) {
-    console.log('my json', data);
+    // console.log('my json', data);
   })
   .catch(function(error) {
     var p = document.createElement('p');
@@ -39,10 +39,10 @@ function updateHtmlWithValues() {
       myExplainList = document.querySelector('#explain_area');
       for(var i = 0; i < data.Headers.length; i++) {
         var listItem = document.createElement('li');
-        listItem.innerHTML = '<strong>' + data.Headers[i].Name + '</strong>';
-        listItem.innerHTML +=' is known for doing <strong> ' + data.Headers[i].Details + '</strong>.';
-        listItem.innerHTML +=' Find more info here: <strong>' + data.Headers[i].URL + '</strong>';
-        myList.appendChild(listItem);
+        // listItem.innerHTML = '<strong>' + data.Headers[i].Name + '</strong>';
+        // listItem.innerHTML +=' is known for doing <strong> ' + data.Headers[i].Details + '</strong>.';
+        // listItem.innerHTML +=' Find more info here: <strong>' + data.Headers[i].URL + '</strong>';
+        myExplainList.replaceWith(listItem, myExplainList);
       }
   });
 };
@@ -54,7 +54,7 @@ function updateHtmlWithCacheExplained(){
   .then(function(data) {
     // get the value input by the user
     inHeaders = document.getElementsByName('input_headers')[0].value;
-    console.log("some thing:  " + inHeaders);
+    // console.log("some thing:  " + inHeaders);
 
     document.getElementById("demo").innerHTML = "YOU CLICKED ME with : " + inHeaders;
     setTimeout(function(){
@@ -62,21 +62,14 @@ function updateHtmlWithCacheExplained(){
     }, 2000);
 
     for(var i = 0; i < data.Headers.length; i++) {
-      console.log(data.Headers[i].Name)
+      // console.log(data.Headers[i].Name)
+      var listItem = document.createElement('li');
       if (data.Headers[i].Name.includes(inHeaders)) {
-        console.log('hit : ', inHeaders, inHeaders.includes('pragma'));
-        var listItem = document.createElement('li');
         listItem.innerHTML = '<strong>' + data.Headers[i].Name + '</strong>';
         listItem.innerHTML +=' is known for doing <strong> ' + data.Headers[i].Details + '</strong>.';
         listItem.innerHTML +=' Find more info here: <strong>' + data.Headers[i].URL + '</strong>';
-        console.log(listItem);
-        myExplainList.appendChild(listItem);
+        myExplainList.replaceWith(listItem, myExplainList);
       }
-        // document.getElementById("explain_area").innerHTML = "I am going to explain what the following header means : " + inHeaders;
     };
-      // if (inHeaders.includes('cache-control')) {
-      //   console.log(inHeaders, inHeaders.includes('cache-control'));
-      //   document.getElementById("explain_area").innerHTML = "I am going to explain what the following header means : " + inHeaders;
-      // };
   });
 };
