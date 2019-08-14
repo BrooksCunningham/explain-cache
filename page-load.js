a775a03737fb0d08d8c7c079efd09840d1d7da26
Brooks Cunningham <22921241+BrooksCunningham@users.noreply.github.com>
2019-08-13 20:55:14 -0400

    Fixing buttons and adding links.
    Also, added the dog for favico.

--- page-load.js
+++ page-load.js
@@ -36,24 +36,29 @@ function updateHtmlWithValues() {
     .then(status)
     .then(json)
     .then(function(data) {
-      myExplainList = document.querySelector('#explain_area');
+      // myExplainList = document.querySelector('#explain_area');
       for(var i = 0; i < data.Headers.length; i++) {
         var listItem = document.createElement('li');
         // listItem.innerHTML = '<strong>' + data.Headers[i].Name + '</strong>';
         // listItem.innerHTML +=' is known for doing <strong> ' + data.Headers[i].Details + '</strong>.';
         // listItem.innerHTML +=' Find more info here: <strong>' + data.Headers[i].URL + '</strong>';
-        myExplainList.replaceWith(listItem, myExplainList);
+        // myExplainList.replaceWith(listItem, myExplainList);
       }
   });
 };
 
-function updateHtmlWithCacheExplained(){
+function updateHtmlWithCacheExplained(headerName){
   fetch('./cache-headers-and-values.json')
   .then(status)
   .then(json)
   .then(function(data) {
     // get the value input by the user
-    inHeaders = document.getElementsByName('input_headers')[0].value;
+    if (document.getElementsByName('input_headers')[0].value.length > 2) {
+      inHeaders = document.getElementsByName('input_headers')[0].value;
+    }
+    else {
+      inHeaders = headerName;
+    };
     // console.log("some thing:  " + inHeaders);
 
     document.getElementById("demo").innerHTML = "YOU CLICKED ME with : " + inHeaders;
@@ -61,14 +66,20 @@ function updateHtmlWithCacheExplained(){
       document.getElementById("demo").innerHTML = "Click me";
     }, 2000);
 
+    myExplainList = document.querySelector('#explain_area');
     for(var i = 0; i < data.Headers.length; i++) {
       // console.log(data.Headers[i].Name)
-      var listItem = document.createElement('li');
+      // var listItem = document.createElement('li');
       if (data.Headers[i].Name.includes(inHeaders)) {
-        listItem.innerHTML = '<strong>' + data.Headers[i].Name + '</strong>';
-        listItem.innerHTML +=' is known for doing <strong> ' + data.Headers[i].Details + '</strong>.';
-        listItem.innerHTML +=' Find more info here: <strong>' + data.Headers[i].URL + '</strong>';
-        myExplainList.replaceWith(listItem, myExplainList);
+        // listItem.innerHTML = '<ul id="explain_area">'
+        // listItem.innerHTML
+        myExplainList.innerHTML = '<strong>' + data.Headers[i].Name + '</strong> is known for doing <strong> ' + data.Headers[i].Details + '</strong>. Find more info here: <strong> '
+        myExplainList.innerHTML += '<a href="' + data.Headers[i].URL + '">' + data.Headers[i].URL + '</a></strong>';
+
+        // listItem.innerHTML += '</ul>'
+        // myExplainList.innerHTML = listItem;
+        // myExplainList.innerHTML
+        // myExplainList.replaceChild(listItem, myExplainList.innerHTML);
       }
     };
   });
